756c6ce078b630cc8157131f4b6b5ab66b12ed2b
jeremiasudesa <jfigueiredopaschmann@udesa.edu.ar>
2023-03-16 17:51:47 -0300

    changed file names

--- TP1/Playlist.hs
+++ TP1/Playlist.hs
@@ -1,13 +1,12 @@
-module Playlist ( Playlist, nuevaP, actualP, skipP, backP, resetP )
-  where
+module Playlist (Playlist, nuevaP, actualP, skipP, backP, resetP) where
 
+import Tema (Tema)
 import Tipos ()
-import Tema ( Tema )
 
-data Playlist = Play Int [ Tema ] deriving (Eq, Show)
+data Playlist = Play Int [Tema] deriving (Eq, Show)
 
-nuevaP :: [ Tema ] -> Playlist
-nuevaP (x:xs) = Play 0 (x:xs)
+nuevaP :: [Tema] -> Playlist
+nuevaP (x : xs) = Play 0 (x : xs)
 
 actualP :: Playlist -> Tema
 actualP (Play indice lista) = lista !! indice
@@ -20,4 +19,3 @@ backP (Play indice lista) = Play (indice - 1) lista
 
 resetP :: Playlist -> Playlist
 resetP (Play int lista) = nuevaP lista
-
---
+++ TP1/Tema.hs
@@ -0,0 +1,25 @@
+module Tema (Tema, nuevoT, nombreT, datosT, etiquetasT, agregarT, aplicaT) where
+
+import Tipos
+
+data Tema = Tem Nombre [Etiqueta] Datos deriving (Eq, Show, Ord)
+
+nuevoT :: Nombre -> Datos -> Tema
+nuevoT nombre = Tem nombre []
+
+nombreT :: Tema -> Nombre
+nombreT (Tem nombre _ _) = nombre
+
+datosT :: Tema -> Datos
+datosT (Tem _ _ datos) = datos
+
+etiquetasT :: Tema -> [Etiqueta]
+etiquetasT (Tem _ etiqueta _) = etiqueta
+
+agregarT :: Etiqueta -> Tema -> Tema
+agregarT etiqueta (Tem nombre _ datos) = Tem nombre [etiqueta] datos -- dudita, no se si esta bienq que haga esto
+
+aplicaT :: Etiqueta -> Tema -> Bool
+aplicaT etiqueta (Tem nom et dat)
+  | [etiqueta] == et = True
+  | otherwise = False
