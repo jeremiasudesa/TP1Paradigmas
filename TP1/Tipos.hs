ddd3ec02b41d32075808d3334f4bc76cb35fe9e4
Francisco Nattero <fnattero@udesa.edu.ar>
2023-03-17 12:01:01 -0300

    modified:   TP1/Tipos.hs

--- TP1/Tipos.hs
+++ TP1/Tipos.hs
@@ -9,9 +9,9 @@ type Nombre = String
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x : xs) =
-  let smallerSorted = quicksort [a | a <- xs, a <= x]
-      biggerSorted = quicksort [a | a <- xs, a > x]
-   in smallerSorted ++ [x] ++ biggerSorted
+  let menoresquehead = quicksort [a | a <- xs, a <= x]
+      mayoresquehead = quicksort [a | a <- xs, a > x]
+   in menoresquehead ++ [x] ++ mayoresquehead
 
 insertar :: Ord a => a -> [a] -> [a]
 insertar elemento lista = quicksort (elemento : lista)
