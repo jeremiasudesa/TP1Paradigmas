e9b9f7c9c961fb659d5b30ff529cb03135c090a4
jeremiasudesa <jfigueiredopaschmann@udesa.edu.ar>
2023-03-24 13:32:02 -0300

    test para tipos y tema funcionando

--- TP1/Tema.hs
+++ TP1/Tema.hs
@@ -5,8 +5,8 @@ import Tipos (Datos, Etiqueta, Nombre)
 data Tema = Tem Nombre [Etiqueta] Datos deriving (Eq, Show, Ord)
 
 -- nuevoT :: Nombre →Datos →Tema
-nuevoT :: Nombre -> [Etiqueta] -> Datos -> Tema -- faltaba etiqueta, preguntar al profe.
-nuevoT nombre datos = Tem nombre []
+nuevoT :: Nombre -> Datos -> Tema
+nuevoT nombre = Tem nombre []
 
 -- nombreT :: Tema →Nombre
 nombreT :: Tema -> Nombre
@@ -18,12 +18,37 @@ datosT (Tem _ _ datos) = datos
 
 -- etiquetasT :: Tema →[ Etiqueta ]
 etiquetasT :: Tema -> [Etiqueta]
-etiquetasT (Tem _ etiqueta _) = etiqueta
+etiquetasT (Tem _ etiquetas _) = etiquetas
 
 -- agregarT :: Etiqueta →Tema →Tema
 agregarT :: Etiqueta -> Tema -> Tema
-agregarT etiqueta (Tem nombre _ datos) = Tem nombre [etiqueta] datos
+agregarT etiqueta (Tem nombre etiquetas datos) = Tem nombre (etiqueta : etiquetas) datos
 
 -- aplicaT :: Etiqueta →Tema →Bool
 aplicaT :: Etiqueta -> Tema -> Bool
 aplicaT etiqueta (Tem _ etiquetaTema _) = etiqueta `elem` etiquetaTema
+
+{-
+TEST
+setup: primero creamos una "cancion test", en base a otras
+variables temporales que nos permiten llegar a un tema
+que tenga sentido testear. Tambien usamos cancionBase1
+para los tests sobre una lista de etiquetas vacia.
+
+1) nos fijamos si nuevoT funciona WLOG
+2) nos fijamos si nombreT funciona WLOG
+3) nos fijamos si datosT funciona WLOG
+4) nos fijamos si devuelve las etiquetas de la cancion cuando la lista no es vacia
+5) 4) pero con lista vacia
+6) nos fijamos que devuelve aplicaT con una etiqueta que no pertenece a la cancion
+7) 6) pero cuando aplica
+8) 6) pero cuando la lista esta vacia
+-}
+
+cancionBase1 = nuevoT "nombre" "dato"
+
+cancionBase2 = agregarT "plunderphonics" cancionBase1
+
+cancionTest = agregarT "bubblegum bass" cancionBase2
+
+testTema = [cancionBase1 == Tem "nombre" [] "dato", nombreT cancionTest == "nombre", datosT cancionTest == "dato", etiquetasT cancionTest == ["bubblegum bass", "plunderphonics"], null (etiquetasT cancionBase1), not (aplicaT "avant garde" cancionTest), aplicaT "plunderphonics" cancionTest, not (aplicaT "plunderphonics" cancionBase1)]
--- TP1/Tipos.hs
+++ TP1/Tipos.hs
@@ -1,17 +1,28 @@
 module Tipos where
 
+import Data.List
+
 type Datos = String
 
 type Etiqueta = String
 
 type Nombre = String
 
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x : xs) =
-  let menoresquehead = quicksort [a | a <- xs, a <= x]
-      mayoresquehead = quicksort [a | a <- xs, a > x]
-   in menoresquehead ++ [x] ++ mayoresquehead
+-- comentamos nuestra definicion del quicksort ya que en la consigna no se aclaraba explicitamente que debiamos implementarlo nosotros
+-- quicksort :: (Ord a) => [a] -> [a]
+-- quicksort [] = []
+-- quicksort (x : xs) =
+--   let menoresquehead = quicksort [a | a <- xs, a <= x]
+--       mayoresquehead = quicksort [a | a <- xs, a > x]
+--    in menoresquehead ++ [x] ++ mayoresquehead
 
 insertar :: Ord a => a -> [a] -> [a]
-insertar elemento lista = quicksort (elemento : lista)
+insertar elemento lista = sort (elemento : lista)
+
+{-
+1) ordenar lista de strings
+2) meter una lista vacia
+3) ordenar lista de Nums
+-}
+
+testTipos = [insertar "tercera" ["ganamos", "la"] == ["ganamos", "la", "tercera"], insertar "hola" [] == ["hola"], insertar 4.0 [3, 1] == [1, 3, 4.0]]
