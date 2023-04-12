ed518b3de7fad9c05dd38b3c44cfdbaf5d546f57
jeremiasudesa <jfigueiredopaschmann@udesa.edu.ar>
2023-04-12 16:03:51 -0300

    d

---
+++ TP2/stack/NodeUtil.java
@@ -0,0 +1,11 @@
+package stack;
+
+public class NodeUtil {
+	int getTraceLenght(Node<NonEmptyComponent> node) {
+		return 1;
+	}
+
+	int getTraceLenght(Node<EmptyComponent> node) {
+		return 1;
+	}
+}
