ae90cbd81f6cf8a689c5098468211850edde50b4
Francisco Nattero <fnattero@udesa.edu.ar>
2023-06-11 15:39:34 -0300

    Co-authored-by: jerecoder <jerecoder@users.noreply.github.com>

--- TP4/src/illipalinret/Game.java
+++ TP4/src/illipalinret/Game.java
@@ -1,5 +1,17 @@
 package illipalinret;
 
 public class Game {
+    Board board;
+    void movePieceX(int pieceIndex, int col, int row){
+        //"primera fase" del jugador esta completada (si ya puso todas sus piezas)
+        //  Player: guardar fase
+        //cada jugador {0, 1, 2}, {3, 4, 5} sabe sus piezas
+        //nos fijamos si la pieza es valida (0, 2)
+        //  Board: nos fijamos si la pieza pertenece al jugador
+        //cambiamos la coordenada de la pieza a {col, row}
+        //  Board: nos fijamos si esta entre las dimensiones del board
+        //nos fijamos si gano
+    }
+
 
 }
